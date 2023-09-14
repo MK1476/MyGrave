a10fcc5a04be8e83a7761ec1d7e16b22942d49d9
MK1476 <99121asif@gmail.com>
2023-09-15 01:58:35 +0530

    First Commit

---
+++ src/main/java/com/mk1476/MyGrave/MyGraveApplication.java
@@ -0,0 +1,13 @@
+package com.mk1476.MyGrave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGraveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGraveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mk1476/MyGrave/MyGraveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mk1476.MyGrave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGraveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
